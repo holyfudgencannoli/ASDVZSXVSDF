4e68d36a2405007fed1a7cdfb981d238e48db85c
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-08-13 23:47:08 -0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    
+    const viewportWidth = window.innerWidth
+    const viewportHeight = window.innerHeight
+        
+    if (window.matchMedia('screen and (min-width: 768px)').matches) {
+        // document.getElementById('toggler').style.display = 'none'
+
+    }
+
+    function showMobileMenu() {
+    
+}
+})
+
